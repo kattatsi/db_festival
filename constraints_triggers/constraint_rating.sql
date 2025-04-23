f337e472decfeb1c9d41d5f368a638f12da21dcd
PinelopiKopanou <65811416+PinelopiKopanou@users.noreply.github.com>
2025-04-23 19:27:51 +0300

    Delete constraints_triggers directory

--- constraints_triggers/constraint_rating.sql
+++
@@ -1,5 +0,0 @@
--- Περιορισμός μοναδικότητας αξιολόγησης ανά επισκέπτη και εμφάνιση
-
-ALTER TABLE Rating
-ADD CONSTRAINT unique_visitor_performance
-UNIQUE (Visitor_Visitor_id, Performance_Performance_id);
--- constraints_triggers/unique_visitor_event.sql
+++
@@ -1,5 +0,0 @@
-
--- Ένας επισκέπτης δεν μπορεί να έχει 2 εισιτήρια για το ίδιο event
-
-ALTER TABLE Ticket
-ADD CONSTRAINT unique_visitor_event UNIQUE (Visitor_Visitor_id, Event_Event_id);
